569a6f3809c99ea4e4bade918d2f5e138eb01108
Kimheaktan <54483773+kimheaktan@users.noreply.github.com>
2020-06-23 09:58:35 -0700

    first

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Fundamental1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for(int i= 1; i<= 255; i++){
+            //     Console.WriteLine(i);
+            // }
+
+            // for(int i=1; i<=100; i++){
+            //     if(i%3 == 0 && i % 5 == 0){
+            //     }
+            //     else if(i % 3 == 0 || i % 5 == 0 ){
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            
+            // for(int i=1; i<=100; i++){
+            //     if(i%3 == 0 && i % 5 == 0){
+            //         Console.WriteLine("FB");
+            //     }
+            //     else if(i % 3 == 0){
+            //         Console.WriteLine("fizz");
+            //     }
+            //     else if( i % 5 == 0){
+            //         Console.WriteLine("buzz");
+            //     }
+                
+            }
+
+            int i = 1;
+            while(i < 100){
+                if(i%3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FB");
+                }
+                else if(i % 3 == 0){
+                    Console.WriteLine("fizz");
+                }
+                else if( i % 5 == 0){
+                    Console.WriteLine("buzz");
+                }
+            }
+
+
+        }
+    }
+}
